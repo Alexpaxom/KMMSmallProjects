7614e704c6704dff68fe4c40a03d31c7225fd780
Pakhomov Alexandr <WorkAlexAn@gmail.com>
2022-04-29 21:53:33 +0300

    Initial

--- src/main/kotlin/CellField.kt
+++ src/main/kotlin/CellField.kt
@@ -1,5 +1,7 @@
 import org.w3c.dom.CanvasRenderingContext2D
-import kotlin.js.Console
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 data class GraphCellCoordinate(val x: Int, val y: Int) {
     val screenX = x * IBoardCell.CELL_WIDTH
@@ -36,14 +38,15 @@ class GraphCellWall(
     }
 }
 
-class GraphCellPathLine(
-    override val coordinate: GraphCellCoordinate
+class GraphCellCustomColorLine(
+    override val coordinate: GraphCellCoordinate,
+    val color: String = "#0000FF"
 ) : IBoardCell {
 
     override fun draw(ctx: CanvasRenderingContext2D) {
         with(ctx) {
             beginPath()
-            fillStyle = "#0000FF"
+            fillStyle = color
             rect(coordinate.screenX, coordinate.screenY, IBoardCell.CELL_WIDTH, IBoardCell.CELL_HEIGHT)
             fill()
             closePath()
@@ -71,14 +74,19 @@ class GraphCellField(val width: Int, val height: Int) : ArrayList<ArrayList<IBoa
     fun clearBoard() {
         for (x in 0 until width)
             for (y in 0 until height)
-                if(this[x][y] is GraphCellPathLine)
+                if (this[x][y] is GraphCellCustomColorLine)
                     this[x][y] = GraphCellEmpty(GraphCellCoordinate(x, y))
     }
 
     fun setListCells(cells: List<IBoardCell>) {
-        for(cell in cells) {
+        for (cell in cells) {
             this[cell.coordinate.x][cell.coordinate.y] = cell
         }
+    }
+
+    fun drawListCells(cxt: CanvasRenderingContext2D, cells: List<IBoardCell>) {
+        for (cell in cells)
+            cell.draw(cxt)
     }
 
     fun drawField(cxt: CanvasRenderingContext2D) {
@@ -90,27 +98,37 @@ class GraphCellField(val width: Int, val height: Int) : ArrayList<ArrayList<IBoa
 
 
 class GraphCellEditor(private val targetBoard: GraphCellField) {
-    fun onClickHandler(xClickPos: Double, yClickPos: Double) {
-        val x = (xClickPos / (IBoardCell.CELL_WIDTH)).toInt()
-        val y = (yClickPos / (IBoardCell.CELL_HEIGHT)).toInt()
 
-
-        when (targetBoard[x][y]) {
+    fun setWallOnField(coordinates: GraphCellCoordinate) {
+        when (targetBoard[coordinates.x][coordinates.y]) {
             is GraphCellWall -> {
-                targetBoard[x][y] =
-                    GraphCellEmpty(GraphCellCoordinate(x, y))
+                targetBoard[coordinates.x][coordinates.y] =
+                    GraphCellEmpty(GraphCellCoordinate(coordinates.x, coordinates.y))
             }
-            is GraphCellEmpty, is GraphCellPathLine -> {
-                targetBoard[x][y] =
-                    GraphCellWall(GraphCellCoordinate(x, y))
+            is GraphCellEmpty, is GraphCellCustomColorLine -> {
+                targetBoard[coordinates.x][coordinates.y] =
+                    GraphCellWall(GraphCellCoordinate(coordinates.x, coordinates.y))
             }
         }
     }
+
+    fun transformScreenCoordinates(xClickPos: Double, yClickPos: Double): GraphCellCoordinate {
+        val x = (xClickPos / (IBoardCell.CELL_WIDTH)).toInt()
+        val y = (yClickPos / (IBoardCell.CELL_HEIGHT)).toInt()
+
+        return GraphCellCoordinate(x, y)
+    }
 }
 
-class GraphNodeInfo(val coordinate: GraphCellCoordinate, val weight: Int = -1, val fromPointCoordinate: GraphNodeInfo? = null): Comparable<GraphNodeInfo> {
+class GraphNodeInfo(
+    val coordinate: GraphCellCoordinate,
+    val weight: Int = -1,
+    val fromPointCoordinate: GraphNodeInfo? = null,
+    val pos: Int = 0,
+    val approximateWeight: Int = -1,
+) : Comparable<GraphNodeInfo> {
     override fun equals(other: Any?): Boolean {
-        return if(other is GraphNodeInfo)
+        return if (other is GraphNodeInfo)
             coordinate == other.coordinate
         else
             false
@@ -124,57 +142,235 @@ class GraphNodeInfo(val coordinate: GraphCellCoordinate, val weight: Int = -1, v
 fun findShortestPath(
     targetBoard: GraphCellField,
     startCoordinate: GraphCellCoordinate,
-    targetCoordinate: GraphCellCoordinate,
-    console: Console
-): List<GraphCellPathLine> {
+    targetCoordinate: GraphCellCoordinate
+): List<GraphNodeInfo> {
     val visitedNodes: MutableSet<GraphCellCoordinate> = mutableSetOf()
     val nodesForVisit: ArrayList<GraphNodeInfo> = arrayListOf()
 
     var currentNode = GraphNodeInfo(startCoordinate, 0, null)
-    var finishNode = GraphNodeInfo(targetCoordinate)
 
-    while (currentNode != finishNode) {
+    while (currentNode.coordinate != targetCoordinate) {
         val listLinkedNodes: List<IBoardCell?> = listOf(
-            targetBoard.getOrNull(currentNode.coordinate.x+1)?.getOrNull(currentNode.coordinate.y),
-            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y+1),
-            targetBoard.getOrNull(currentNode.coordinate.x-1)?.getOrNull(currentNode.coordinate.y),
-            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y-1)
+            targetBoard.getOrNull(currentNode.coordinate.x + 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y + 1),
+            targetBoard.getOrNull(currentNode.coordinate.x - 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y - 1)
         )
 
-        for(graphCell in listLinkedNodes) {
-            if(graphCell == null || graphCell.coordinate in visitedNodes)
+        for (graphCell in listLinkedNodes) {
+            if (graphCell == null || graphCell.coordinate in visitedNodes)
                 continue
 
-            if(graphCell !is GraphCellEmpty) {
+            if (graphCell !is GraphCellEmpty) {
                 visitedNodes.add(graphCell.coordinate)
                 continue
             }
 
             val nodeInx = nodesForVisit.indexOfFirst { it.coordinate == graphCell.coordinate }
-            if(nodeInx == -1)
+            if (nodeInx == -1)
                 nodesForVisit.add(GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode))
-            else if(nodesForVisit[nodeInx].weight > currentNode.weight + 1)
+            else if (nodesForVisit[nodeInx].weight > currentNode.weight + 1)
                 nodesForVisit[nodeInx] = GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode)
         }
 
         visitedNodes.add(currentNode.coordinate)
 
         nodesForVisit.sort()
-        if(nodesForVisit.isNotEmpty())
+        if (nodesForVisit.isNotEmpty())
             currentNode = nodesForVisit.removeAt(0)
         else break
     }
 
-    if(finishNode.coordinate != currentNode.coordinate)
+    if (targetCoordinate != currentNode.coordinate)
         return listOf()
 
-    val resultPathList: ArrayList<GraphCellPathLine> = arrayListOf()
+    val resultPathList: ArrayList<GraphNodeInfo> = arrayListOf()
 
     while (true) {
-        resultPathList.add(GraphCellPathLine(currentNode.coordinate))
+        resultPathList.add(currentNode)
         currentNode = currentNode.fromPointCoordinate ?: break
     }
 
     return resultPathList
 }
 
+fun calcShortestPathInfo(
+    targetBoard: GraphCellField,
+    startCoordinate: GraphCellCoordinate,
+    targetCoordinate: GraphCellCoordinate
+): List<GraphNodeInfo> {
+    val visitedNodes: MutableSet<GraphCellCoordinate> = mutableSetOf()
+    val nodesForVisit: ArrayList<GraphNodeInfo> = arrayListOf()
+    val pathInfo: ArrayList<GraphNodeInfo> = arrayListOf()
+
+    var currentNode = GraphNodeInfo(startCoordinate, 0, null)
+    var counter = 0
+    while (currentNode.coordinate != targetCoordinate) {
+        val listLinkedNodes: List<IBoardCell?> = listOf(
+            targetBoard.getOrNull(currentNode.coordinate.x + 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y + 1),
+            targetBoard.getOrNull(currentNode.coordinate.x - 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y - 1)
+        )
+
+        for (graphCell in listLinkedNodes) {
+            if (graphCell == null || graphCell.coordinate in visitedNodes)
+                continue
+
+            if (graphCell !is GraphCellEmpty) {
+                visitedNodes.add(graphCell.coordinate)
+                continue
+            }
+
+            val nodeInx = nodesForVisit.indexOfFirst { it.coordinate == graphCell.coordinate }
+            if (nodeInx == -1) {
+                nodesForVisit.add(GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode, counter))
+                counter++
+            }
+            else if (nodesForVisit[nodeInx].weight > currentNode.weight + 1)
+                nodesForVisit[nodeInx] = GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode, currentNode.pos)
+        }
+
+        visitedNodes.add(currentNode.coordinate)
+
+        nodesForVisit.sort()
+        if (nodesForVisit.isNotEmpty()) {
+            pathInfo.add(currentNode)
+            currentNode = nodesForVisit.removeAt(0)
+        }
+        else break
+    }
+
+    return pathInfo
+}
+
+fun calcShortestPathAstar(
+    targetBoard: GraphCellField,
+    startCoordinate: GraphCellCoordinate,
+    targetCoordinate: GraphCellCoordinate
+): List<GraphNodeInfo> {
+    val visitedNodes: MutableSet<GraphCellCoordinate> = mutableSetOf()
+    val nodesForVisit: ArrayList<GraphNodeInfo> = arrayListOf()
+
+    var currentNode = GraphNodeInfo(startCoordinate, 0, null, manhattanDistance(startCoordinate, targetCoordinate))
+
+    while (currentNode.coordinate != targetCoordinate) {
+        val listLinkedNodes: List<IBoardCell?> = listOf(
+            targetBoard.getOrNull(currentNode.coordinate.x + 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y + 1),
+            targetBoard.getOrNull(currentNode.coordinate.x - 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y - 1)
+        )
+
+        for (graphCell in listLinkedNodes) {
+            if (graphCell == null || graphCell.coordinate in visitedNodes)
+                continue
+
+            if (graphCell !is GraphCellEmpty) {
+                visitedNodes.add(graphCell.coordinate)
+                continue
+            }
+
+            val nodeInx = nodesForVisit.indexOfFirst { it.coordinate == graphCell.coordinate }
+            if (nodeInx == -1)
+                nodesForVisit.add(GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode, 0, manhattanDistance(graphCell.coordinate, targetCoordinate)+currentNode.weight + 1))
+            else if (nodesForVisit[nodeInx].weight > currentNode.weight + 1)
+                nodesForVisit[nodeInx] = GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode, 0, manhattanDistance(graphCell.coordinate, targetCoordinate)+currentNode.weight + 1)
+        }
+
+        visitedNodes.add(currentNode.coordinate)
+
+        nodesForVisit.sortBy { it.approximateWeight }
+        if (nodesForVisit.isNotEmpty())
+            currentNode = nodesForVisit.removeAt(0)
+        else break
+    }
+
+    if (targetCoordinate != currentNode.coordinate)
+        return listOf()
+
+    val resultPathList: ArrayList<GraphNodeInfo> = arrayListOf()
+
+    while (true) {
+        resultPathList.add(currentNode)
+        currentNode = currentNode.fromPointCoordinate ?: break
+    }
+
+    return resultPathList
+}
+
+fun calcShortestPathInfoAstar(
+    targetBoard: GraphCellField,
+    startCoordinate: GraphCellCoordinate,
+    targetCoordinate: GraphCellCoordinate
+): List<GraphNodeInfo> {
+    val visitedNodes: MutableSet<GraphCellCoordinate> = mutableSetOf()
+    val nodesForVisit: ArrayList<GraphNodeInfo> = arrayListOf()
+    val pathInfo: ArrayList<GraphNodeInfo> = arrayListOf()
+
+    var currentNode = GraphNodeInfo(startCoordinate, 0, null, manhattanDistance(startCoordinate, targetCoordinate))
+    var counter = 0
+    while (currentNode.coordinate != targetCoordinate) {
+        val listLinkedNodes: List<IBoardCell?> = listOf(
+            targetBoard.getOrNull(currentNode.coordinate.x + 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y + 1),
+            targetBoard.getOrNull(currentNode.coordinate.x - 1)?.getOrNull(currentNode.coordinate.y),
+            targetBoard.getOrNull(currentNode.coordinate.x)?.getOrNull(currentNode.coordinate.y - 1)
+        )
+
+        for (graphCell in listLinkedNodes) {
+            if (graphCell == null || graphCell.coordinate in visitedNodes)
+                continue
+
+            if (graphCell !is GraphCellEmpty) {
+                visitedNodes.add(graphCell.coordinate)
+                continue
+            }
+
+            val nodeInx = nodesForVisit.indexOfFirst { it.coordinate == graphCell.coordinate }
+            if (nodeInx == -1) {
+                nodesForVisit.add(
+                    GraphNodeInfo(
+                        graphCell.coordinate,
+                        currentNode.weight + 1,
+                        currentNode,
+                        counter,
+                        manhattanDistance(graphCell.coordinate, targetCoordinate)+currentNode.weight + 1
+                    )
+                )
+                counter++
+            }
+            else if (nodesForVisit[nodeInx].weight > currentNode.weight + 1)
+                nodesForVisit[nodeInx] = GraphNodeInfo(graphCell.coordinate, currentNode.weight + 1, currentNode, currentNode.pos, manhattanDistance(graphCell.coordinate, targetCoordinate)+currentNode.weight + 1)
+        }
+
+        visitedNodes.add(currentNode.coordinate)
+
+        nodesForVisit.sortBy { it.approximateWeight }
+        if (nodesForVisit.isNotEmpty()) {
+            pathInfo.add(currentNode)
+            currentNode = nodesForVisit.removeAt(0)
+        }
+        else break
+    }
+
+    return pathInfo
+}
+//euclideanDistance
+fun euclideanDistance(
+    startCoordinate: GraphCellCoordinate,
+    targetCoordinate: GraphCellCoordinate
+): Int {
+    return  sqrt(
+        (startCoordinate.x-targetCoordinate.x).toDouble().pow(2)
+            +
+            (startCoordinate.y-targetCoordinate.y).toDouble().pow(2)
+        ).toInt()
+}
+//manhattanDistance
+fun manhattanDistance(
+    startCoordinate: GraphCellCoordinate,
+    targetCoordinate: GraphCellCoordinate
+): Int {
+    return abs(startCoordinate.x-targetCoordinate.x) + abs(startCoordinate.y-targetCoordinate.y)
+}
--- src/main/kotlin/simple.kt
+++ src/main/kotlin/simple.kt
@@ -1,42 +1,164 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.KeyboardEvent
 
-fun main() {
+class Game {
+
+    var currentGameStatus: GameStatus = WallsSet()
+
     val canvas = document.getElementById("myCanvas") as HTMLCanvasElement
     val graphCellField = GraphCellField(50,50)
     val graphFieldEditor = GraphCellEditor(graphCellField)
     val ctx = canvas.getContext("2d") as CanvasRenderingContext2D
-    var result = findShortestPath(graphCellField, GraphCellCoordinate(30,40), GraphCellCoordinate(4,5), console)
-    var counter = -1
+    var shortestPath: List<GraphNodeInfo> = listOf()
+    var shortestPathInfo: List<GraphNodeInfo> = listOf()
+    var startPoint = GraphCellCoordinate(7,7)
+    var endPoint = GraphCellCoordinate(4,5)
+    var counter = 0
+    var startDrawWall = false
+    val pathInfoDrawLine: ArrayList<GraphCellCustomColorLine> = arrayListOf()
+    var prevCoordinate: GraphCellCoordinate?= null
+    var currentIntervalFunctionId: Int = 0
 
-    window.onload = {
-
-        canvas.onmousedown = { e->
-                graphFieldEditor.onClickHandler(e.offsetX, e.offsetY)
-                graphCellField.clearBoard()
-                result = findShortestPath(graphCellField, GraphCellCoordinate(30,40), GraphCellCoordinate(4,5), console)
-                console.log(result)
+    init {
+        window.onload = {
+            setListeners()
+            currentIntervalFunctionId = setIntervalFunction()
+            Unit
         }
 
-        document.addEventListener("keydown", { event ->
-            if(event is KeyboardEvent) {
-                console.log("${event.keyCode}")
-            }
-        })
     }
 
-    window.setInterval({
-        counter = (counter + 1) % result.size
-        result.getOrNull(counter)?.let{
-        graphCellField.setListCells(listOf(
-            it
-        ))
+    fun setListeners() {
+        canvas.onmousedown = { e->
+            val clickCoordinate = graphFieldEditor.transformScreenCoordinates(e.offsetX, e.offsetY)
+
+            when(currentGameStatus) {
+                is WallsSet ->{
+                    startDrawWall = true
+                    graphFieldEditor.setWallOnField(clickCoordinate)
+                    prevCoordinate = clickCoordinate
+                }
+                is StartPointSet -> startPoint = clickCoordinate
+                is EndPointSet -> endPoint = clickCoordinate
+            }
+
+            Unit
         }
 
-        graphCellField.drawField(ctx)
-    }, 10)
+        canvas.onmouseup = { e->
+            when(currentGameStatus) {
+                is WallsSet -> startDrawWall = false
+            }
+            Unit
+        }
+
+
+
+        canvas.onmousemove = { e->
+
+            val moveCoordinate = graphFieldEditor.transformScreenCoordinates(e.offsetX, e.offsetY)
+
+            if(startDrawWall && (prevCoordinate == null || prevCoordinate != moveCoordinate)) {
+
+                prevCoordinate = moveCoordinate
+
+                when (currentGameStatus) {
+                    is WallsSet -> graphFieldEditor.setWallOnField(moveCoordinate)
+                }
+            }
+            Unit
+        }
+
+        (document.getElementById("display") as HTMLButtonElement).addEventListener("click", {
+            currentGameStatus = Started(true)
+            graphCellField.drawField(ctx)
+            shortestPath = calcShortestPathAstar(graphCellField, startPoint, endPoint)
+            shortestPathInfo = calcShortestPathInfoAstar(graphCellField, startPoint, endPoint)
+            counter = 0
+            pathInfoDrawLine.clear()
+            console.log("clicked")
+        })
+
+        (document.getElementById("set_start_point") as HTMLButtonElement).addEventListener("click", {
+            currentGameStatus = StartPointSet()
+        })
+
+        (document.getElementById("set_end_point") as HTMLButtonElement).addEventListener("click", {
+            currentGameStatus = EndPointSet()
+        })
+
+        (document.getElementById("set_wall") as HTMLButtonElement).addEventListener("click", {
+            currentGameStatus = WallsSet()
+        })
+
+        (document.getElementById("clear") as HTMLButtonElement).addEventListener("click", {
+            counter = 0
+            shortestPath = listOf()
+            shortestPathInfo = listOf()
+            pathInfoDrawLine.clear()
+        })
+
+        (document.getElementById("display_frequency") as HTMLInputElement).let { displayFrequencyRange ->
+            displayFrequencyRange.addEventListener("change", {
+                window.clearInterval(currentIntervalFunctionId)
+                currentIntervalFunctionId = setIntervalFunction((MAX_TIMEOUT+1 - displayFrequencyRange.valueAsNumber/100*MAX_TIMEOUT).toInt())
+                console.log((MAX_TIMEOUT+1 - displayFrequencyRange.valueAsNumber/100*MAX_TIMEOUT).toInt())
+            })
+        }
+
+        //        document.addEventListener("keydown", { event ->
+        //            if(event is KeyboardEvent) {
+        //                console.log("${event.keyCode}")
+        //            }
+        //        })
+    }
+
+    fun setIntervalFunction(drawFrequencyMilliseconds: Int = MAX_TIMEOUT/2): Int {
+
+        return window.setInterval({
+            graphCellField.drawField(ctx)
+
+            if(shortestPathInfo.isNotEmpty() && counter<shortestPathInfo.size) {
+                pathInfoDrawLine.add(GraphCellCustomColorLine(shortestPathInfo[counter].coordinate))
+                counter++
+            }
+
+            graphCellField.drawListCells(ctx, pathInfoDrawLine)
+
+            if(counter>=shortestPathInfo.size) {
+                graphCellField.drawListCells(
+                    ctx,
+                    shortestPath.map { GraphCellCustomColorLine(it.coordinate, "#FFFF00") })
+                if(currentGameStatus is Started)
+                    currentGameStatus = WallsSet()
+            }
+
+        graphCellField.drawListCells(ctx, listOf(
+            GraphCellCustomColorLine(startPoint, "#FF0000"),
+            GraphCellCustomColorLine(endPoint, "#FFFFFF")
+        ))
+
+        }, drawFrequencyMilliseconds)
+    }
+
+    companion object {
+        const val MAX_TIMEOUT = 1000
+    }
 
 }
+
+fun main() {
+    Game()
+}
+
+sealed interface GameStatus
+
+class Started(val showCalculation: Boolean): GameStatus
+class StartPointSet(): GameStatus
+class EndPointSet(): GameStatus
+class WallsSet(): GameStatus
